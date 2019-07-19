4b57ae143f46dc6f10956c50fed7cd2f31223d63
Grism Patel <grismpatel@hotmail.com>
2019-07-19 14:57:56 -0400

    added logs and try/catch for easy debugs

--- test.py
+++ test.py
@@ -23,8 +23,20 @@ with open(path + '/Glottis/sequential_config.json') as json_file:
 
 model = Sequential()
 
-first_layer = data['first_layer']
-layer_type = data[first_layer]['type']
+try:
+    first_layer = data['first_layer']
+except KeyError:
+    raise KeyError ('config does not contain first_layer. Please add it.')
+
+try:
+    data[first_layer]
+except KeyError:
+    raise KeyError ('first layer object is missing in the config. Please add it.')
+
+try:
+    layer_type = data[first_layer]['type']
+except KeyError:
+    raise KeyError ('first layer config type is missing. Please add it.')
 
 print ('Current Layer is {layer} of type: {layer_type}'.format(layer_type = layer_type, layer = first_layer))
 model.add(Conv2D(data[first_layer]['number_kernels'],
@@ -37,8 +49,18 @@ model.add(Conv2D(data[first_layer]['number_kernels'],
 current_layer = data[first_layer]['next']
 
 while True:
-    layer_type = data[current_layer]['type']
+    try:
+        data[current_layer]
+    except KeyError:
+        raise KeyError ('{current_layer} object is not defined in the config. Please add it'.format(current_layer = current_layer))
+
+    try:
+        layer_type = data[current_layer]['type']
+    except KeyError:
+        raise KeyError ('{current_layer} object has no type in the config. Please add it.'.format(current_layer = current_layer))
+
     print ('Current Layer is {layer} of type: {layer_type}'.format(layer_type = layer_type, layer = current_layer))
+    
     if layer_type == 'conv2d':
         model.add(Conv2D(data[current_layer]['number_kernels'],
                          (data[current_layer]['kernel_width'], data[current_layer]['kernel_height']),
@@ -46,16 +68,19 @@ while True:
                          kernel_initializer = tf.initializers.variance_scaling(scale= data[current_layer]['kernel_initializer_scale']),
                          kernel_regularizer = tf.contrib.layers.l2_regularizer(scale= data[current_layer]['kernel_regularizer_scale'])))
     
-    if layer_type == 'maxpool2d':
+    elif layer_type == 'maxpool2d':
         model.add(MaxPool2D(pool_size = (data[current_layer]['pool_width'], data[current_layer]['pool_height'])))
 
-    if layer_type == 'dense':
+    elif layer_type == 'dense':
         model.add(Dense(units = data[current_layer]['units'],
                         activation = data[current_layer]['activation']))
 
-    if layer_type == 'flatten':
+    elif layer_type == 'flatten':
         model.add(Flatten())
 
+    else:
+        raise ValueError ('Invalid layer type in the config. layer_type {layer_type} is not defined in the code.'.format(layer_type = layer_type))
+    
     current_layer = data[current_layer]['next']
     
     if current_layer == None:
@@ -85,10 +110,20 @@ test_set = test_datagen.flow_from_directory(
         class_mode='binary')
 
 result_path = path + '/Glottis/Results/{time}/'.format(time = datetime.datetime.now())
-os.makedirs(result_path)
+
+try:
+    os.makedirs(result_path)
+except OSError:
+    raise OSError ('Failed to create path')
+
 plot_model(model, to_file = result_path + 'GlottisCNN.png', show_shapes = True)
+print ('Model saved successfully to path: {result_path}GlottisCNN.png'.format(result_path = result_path))
+
 csv_logger = CSVLogger(result_path + 'GlottisCNN.csv', append=True, separator='|')
+print ('Logs saved successfully to path: {result_path}GlottisCNN.csv'.format(result_path = result_path))
+
 copyfile(path + '/Glottis/sequential_config.json', result_path + 'config.json')
+print ('Copied the config sucessfully to path: {result_path}config.json').format(result_path = result_path)
 
 model.fit_generator(
         training_set,
